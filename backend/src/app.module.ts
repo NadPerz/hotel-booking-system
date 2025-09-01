ca535c72fb01c3c0b8fbb2b05ab2d0f120de9c3b
AmzalFoumi <147682418+AmzalFoumi@users.noreply.github.com>
2025-09-01 23:58:15 +0530

    Social Module Import

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItineraryModule } from './itinerary/itinerary.module';
+import { SocialModule } from './social/social.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
@@ -18,6 +19,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       inject: [ConfigService],
     }),
     ItineraryModule,
+    SocialModule,
   ],
   controllers: [AppController],
   providers: [AppService],
