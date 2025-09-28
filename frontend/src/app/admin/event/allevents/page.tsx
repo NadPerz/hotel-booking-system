d5556e5b269ab4cc61997e16846cdb7a3f86742f
RazeenFaiz <razeenfaiz49@gmail.com>
2025-09-28 12:04:52 +0530

    added dhasboard

--- frontend/src/app/admin/event/allevents/page.tsx
+++ frontend/src/app/admin/event/allevents/page.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import EventList from '@/features/event/allevents/EventList';
 
 export default function Page() {
-  return <EventList />;
+  return (
+  
+
+      <EventList />
+
+  );
 }
 
 
---
+++ frontend/src/features/event/components/app-sidebar.tsx
@@ -0,0 +1,198 @@
+"use client"
+
+import * as React from "react"
+import {
+  IconCamera,
+  IconChartBar,
+  IconDashboard,
+  IconDatabase,
+  IconFileAi,
+  IconFileDescription,
+  IconFileWord,
+  IconFolder,
+  IconHelp,
+  IconInnerShadowTop,
+  IconListDetails,
+  IconReport,
+  IconSearch,
+  IconSettings,
+  IconUsers,
+  IconCalendarEvent
+} from "@tabler/icons-react"
+
+import { NavDocuments } from "@/features/event/components/nav-documents"
+import { NavMain } from "@/features/event/components/nav-main"
+import { NavSecondary } from "@/features/event/components/nav-secondary"
+import { NavUser } from "@/features/event/components/nav-user"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  navMain: [
+    {
+      title: "Dashboard",
+      url: "#",
+      icon: IconDashboard,
+    },
+    {
+      title: "Calendar",
+      url: "#",
+      icon: IconCalendarEvent,
+    },
+    {
+      title: "Events",
+      url: "/admin/event/allevents",
+      icon: IconListDetails,
+    },
+    {
+      title: "Venues",
+      url: "/admin/event/venues",
+      icon: IconChartBar,
+    },
+    {
+      title: "Organizers",
+      url: "/admin/event/organizers",
+      icon: IconFolder,
+    },
+    {
+      title: "Categories",
+      url: "/admin/event/categories",
+      icon: IconFolder,
+    },
+    {
+      title: "HashTags",
+      url: "/admin/event/hashtags",
+      icon: IconUsers,
+    },
+    {
+      title: "RSVPs",
+      url: "/admin/event/rsvps",
+      icon: IconUsers,
+    },
+    
+  ],
+  navClouds: [
+    {
+      title: "Capture",
+      icon: IconCamera,
+      isActive: true,
+      url: "#",
+      items: [
+        {
+          title: "Active Proposals",
+          url: "#",
+        },
+        {
+          title: "Archived",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Proposal",
+      icon: IconFileDescription,
+      url: "#",
+      items: [
+        {
+          title: "Active Proposals",
+          url: "#",
+        },
+        {
+          title: "Archived",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Prompts",
+      icon: IconFileAi,
+      url: "#",
+      items: [
+        {
+          title: "Active Proposals",
+          url: "#",
+        },
+        {
+          title: "Archived",
+          url: "#",
+        },
+      ],
+    },
+  ],
+  navSecondary: [
+    {
+      title: "Settings",
+      url: "#",
+      icon: IconSettings,
+    },
+    {
+      title: "Get Help",
+      url: "#",
+      icon: IconHelp,
+    },
+    {
+      title: "Search",
+      url: "#",
+      icon: IconSearch,
+    },
+  ],
+  documents: [
+    {
+      name: "Data Library",
+      url: "#",
+      icon: IconDatabase,
+    },
+    {
+      name: "Reports",
+      url: "#",
+      icon: IconReport,
+    },
+    {
+      name: "Word Assistant",
+      url: "#",
+      icon: IconFileWord,
+    },
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="offcanvas" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              asChild
+              className="data-[slot=sidebar-menu-button]:!p-1.5"
+            >
+              <a href="#">
+                <IconInnerShadowTop className="!size-5" />
+                <span className="text-base font-semibold">Itinerary AI</span>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+        <NavDocuments items={data.documents} />
+        <NavSecondary items={data.navSecondary} className="mt-auto" />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
